919f1e5403755c4244bad4ef59df2c6f51cb7a0a
Krishna <krishnakeec@gmail.com>
2020-08-09 16:10:16 +0300

    Left container navigation bar making and styling

--- tna-app/src/LeftContainer.js
+++ tna-app/src/LeftContainer.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import logo_bg from './images/logo_bg.png';
 import logo_main from './images/logo_main.png';
+import bgTNA from './images/bgTNA.png';
 import'./App.css';
 
 export default function LeftContainer() {
   return (
     <div className="LeftContainer">
+     
       <div className="logo">
-      
         <img className="bglogo"src={require('./images/logo_main.png')} /> 
       </div>
+      <div>
+          <div class="containerTNA">
+          <img src={require('./images/bgTNA.png')} alt="logo of TNA" ></img>
+          <h1 class="centeredTNA">Turku Nepal Association Ry</h1>
+          </div>
+          <div class="containerGTK">
+          <img src={require('./images/menu_bg.png')} alt="logo of TNA" ></img>
+          <h1 class="centeredTNA">Good to know</h1>
+          </div>
+          <div class="containerEvents">
+          <img src={require('./images/menu_bg.png')} alt="logo of TNA" ></img>
+          <h1 class="centeredTNA">Events</h1>
+          </div>
+          <div class="containerGallery">
+          <img src={require('./images/menu_bg.png')} alt="logo of TNA" ></img>
+          <h1 class="centeredTNA">Gallery</h1>
+          </div>
+          <div class="containercontact">
+          <img src={require('./images/menu_bg.png')} alt="logo of TNA" ></img>
+          <h1 class="centeredTNA">Contact US</h1>
+          </div>
 
-    </div>
+      </div>
+    
+      
+    </div>  
+
+    
   
   )
 }
