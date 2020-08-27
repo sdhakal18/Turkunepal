bde62222e2f1ff2408d6fc32f2f502f65cd99d27
Krishna <krishnakeec@gmail.com>
2020-08-27 13:51:24 +0300

    used flex and background color for navigation left

--- tna-app/src/LeftContainer.js
+++ tna-app/src/LeftContainer.js
@@ -15,20 +15,22 @@ import {Link, Route, Router} from 'react-router'
 export default function LeftContainer() {
   return (
     <div className="LeftContainer">
-        <div classname="logoName">
-        <div className="logo"><h1>Logo</h1></div>
-        <div className="name"><h1>Turku Nepal Association Ry</h1></div>
-      </div>
+        <div className="flexmenu">
+                <div classname="index">
+                <div className="logo">Logo</div>
+                <div className="name">Turku Nepal Association Ry</div>
+              </div>
 
-          <div className="Pages1">
-            <div className="gtk"><span>Good to know</span></div>
-            <div className="events"><span>Events</span></div>
-            <div className="members"><span>Members</span></div>
-          </div>
-          
-      <div className="pages2">
-            <div className="gallery"><span>Gallery</span></div>
-            <div className="contact"><span>Contact US</span></div>
+                <div className="Pages1">
+                  <div className="gtk"><span>Good to know</span></div>
+                  <div className="events"><span>Events</span></div>
+                  <div className="members"><span>Members</span></div>
+                </div>
+
+            <div className="pages2">
+                  <div className="gallery"><span>Gallery</span></div>
+                  <div className="contact"><span>Contact US</span></div>
+            </div>
       </div>
     </div>  
 
